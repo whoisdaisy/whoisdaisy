49c9d909f77ced9c635d1dd4a0e30235eb96c9b8
Michael Rode <michael@ongress.com>
2025-03-13 11:26:57 +0100

    Add favicons and site-manifest

--- lib/whoisdaisy_web.ex
+++ lib/whoisdaisy_web.ex
@@ -17,7 +17,7 @@ defmodule WhoisdaisyWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt site.webmanifest)
 
   def router do
     quote do
